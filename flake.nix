10f0d2bbc40d0b67809bb2d88093c71d8d4524d0
Pierre Gambarotto <pierre.gambarotto@math.univ-toulouse.fr>
2023-11-09 09:20:54 +0100

    xymon client with hard coded server ip
    157.136.141.40

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
     in
-      {
-
-        packages.${system}.hello = pkgs.stdenv.mkDerivation {
+    {
+      packages.${system} = {
+        hello = pkgs.stdenv.mkDerivation {
           pname = "hello";
           version = "2.12.1";
 
@@ -19,6 +20,27 @@
 
           doCheck = false;
         };
-
+        xymon = pkgs.stdenv.mkDerivation {
+          pname = "xymon";
+          version = "4.3.30";
+          buildInputs = with pkgs;[ pcre libtirpc];
+          CONFTYPE = "client";
+          XYMONUSER="$(whoami)";
+          XYMONTOPDIR="$(out)";
+          XYMONHOSTIP = "157.136.141.40";
+          PKGBUILD = "yes";
+          configureFlags = [
+            "--client"
+            "--pcreinclude ${pkgs.pcre}/include"
+            "--pcrelib ${pkgs.pcre}/lib"
+            "--make ${pkgs.gnumake}/bin/make"
+          ];
+          dontAddPrefix = true;
+          src = builtins.fetchurl {
+            url = "https://netcologne.dl.sourceforge.net/project/xymon/Xymon/4.3.30/xymon-4.3.30.tar.gz";
+            sha256 = "sha256:1xgm3ch2aqlmmkny3805c47ap1hjl9hjq1r5czwmvqg1r1qigmcf";
+          };
+        };
       };
+    };
 }
